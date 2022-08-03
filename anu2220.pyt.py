35e2f35f8e294ec6e9048d558303af84ce80fa4b
anubasavaraj3125 <110526861+anubasavaraj3125@users.noreply.github.com>
2022-08-03 01:35:49 -0700

    Update anu2220.pyt.py

--- anu2220.pyt.py
+++ anu2220.pyt.py
@@ -4,7 +4,7 @@ def recur_fibo(n):
     else:
         return(recur_fibo(n-1)+recur_fibo(n-2))
     nterms=10
-    if nterms<=0:
+    if nterms<=1:
         print("Please enter a positive integer:")
     else:
         print("Fibonnaci sequence:")
